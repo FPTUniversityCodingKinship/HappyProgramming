e5301886522f32aa860a366dc4ad5c54e762f392
ntrphongse <n.tranphongse@gmail.com>
2021-05-26 21:45:54 +0700

    Update UsersDAO
    Add getUserInfo method

--- src/java/hps/users/UsersDAO.java
+++ src/java/hps/users/UsersDAO.java
@@ -56,5 +56,46 @@ public class UsersDAO implements Serializable {
 
         return false;
     }
+    
+    public UsersDTO getUserData(String userID) 
+                throws NamingException, SQLException {
+        Connection con = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            //1.Establish Connection
+            con = DBHelper.makeConnection();
+            if (con != null) {
+                //2. Prepare sql string
+                String sql = "SELECT * "
+                        + "FROM users "
+                        + "WHERE userID = ?";
+                stm = con.prepareStatement(sql);
+                stm.setString(1, userID);
+                //3. Store in ResultSet
+                rs = stm.executeQuery();
+                if (rs.next()) {
+                    UsersDTO dto = new UsersDTO();
+                    dto = new UsersDTO(
+                            rs.getString("userID"), rs.getString("username"),
+                            rs.getString("password"), rs.getString("email"),
+                            rs.getString("fullname"), rs.getString("phone"),
+                            rs.getString("address"), rs.getDate("dob"),
+                            rs.getString("sex"), rs.getString("image") ,
+                            rs.getBoolean("status"), rs.getBoolean("emailStatus")
+                    );
+                    return dto;
+                }
+            }
+        } finally {
+            if (stm != null) {
+                stm.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+        return null;
+    }
 
 }
