3be407da3062da6c2caf565967989693e5872720
ntrphongse <n.tranphongse@gmail.com>
2021-05-27 21:30:02 +0700

    Update RequestsDAO

--- src/java/hps/requests/RequestsDAO.java
+++ src/java/hps/requests/RequestsDAO.java
@@ -118,14 +118,15 @@ public class RequestsDAO implements Serializable {
 
             if (con != null) {
                 String sql = "SELECT requestID, menteeID, mentorID, skillsID, "
-                        + "deadline, title, content, status, openedTime, "
+                        + "deadline, title, reqContent, status, openedTime, "
                         + "approvedTime, canceledTime, closedTime "
                         + "FROM requests "
-                        + "WHERE mentorID = ? AND status = ?";
+                        + "WHERE mentorID = ? AND (status = ? OR status = ?)";
 
                 stmt = con.prepareStatement(sql);
                 stmt.setString(1, mentorID);
-                stmt.setString(3, "P");
+                stmt.setString(2, "P");
+                stmt.setString(3, "A");
 
                 rs = stmt.executeQuery();
                 while (rs.next()) {
@@ -321,8 +322,8 @@ public class RequestsDAO implements Serializable {
                 stmt.setTimestamp(2, new Timestamp(System.currentTimeMillis()));
                 stmt.setString(3, requestID);
                 
-                boolean iCheck = stmt.execute();
-                if (iCheck) {
+                int iCheck = stmt.executeUpdate();
+                if (iCheck > 0) {
                     result = true;
                 }
             }
@@ -419,8 +420,8 @@ public class RequestsDAO implements Serializable {
                 stmt.setTimestamp(2, new Timestamp(System.currentTimeMillis()));
                 stmt.setString(3, requestID);
                 
-                boolean iCheck = stmt.execute();
-                if (iCheck) {
+                int iCheck = stmt.executeUpdate();
+                if (iCheck > 0) {
                     result = true;
                 }
             }
@@ -581,5 +582,40 @@ public class RequestsDAO implements Serializable {
             }
         }
         return totalMentor;
+    }
+    
+    public boolean closeRequest(String requestID) 
+            throws SQLException, NamingException {
+        boolean result = false;
+        
+        Connection con = null;
+        PreparedStatement stmt = null;
+        
+        try {
+            con = DBHelper.makeConnection();
+            if (con != null) {
+                String sql = "UPDATE requests "
+                        + "SET status = ?, closedTime = ? "
+                        + "WHERE requestID = ?";
+                stmt = con.prepareStatement(sql);
+                stmt.setString(1, "C");
+                stmt.setTimestamp(2, new Timestamp(System.currentTimeMillis()));
+                stmt.setString(3, requestID);
+                
+                int iCheck = stmt.executeUpdate();
+                if (iCheck > 0) {
+                    result = true;
+                }
+            }
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+        
+        return result;
     }
 }
