87295029f44e39db5bff212f29ac7065ccb0a08d
ntrphongse <n.tranphongse@gmail.com>
2021-06-02 22:05:28 +0700

    Update UsersDAO
    Add updateProfile with the parameters: userID, fullname, address, dob and sex (based on the createCV page)

--- src/java/hps/users/UsersDAO.java
+++ src/java/hps/users/UsersDAO.java
@@ -186,4 +186,41 @@ public class UsersDAO implements Serializable {
     public UsersDTO newUser(String username, String password) {
         return null;
     }
+    
+    public boolean updateProfile(String userID, String fullname, String address, 
+            Date dob, String sex)
+            throws SQLException, NamingException {
+
+        //1. Establish DB connection
+        Connection con = null;
+        PreparedStatement stm = null;
+        try {
+            con = DBHelper.makeConnection();
+            if (con != null) {
+                //2. Prepare SQL string
+                String sql = "UPDATE users "
+                        + "SET fullname = ?, address = ?, "
+                        + "dob = ? , sex = ? "
+                        + "WHERE userID = ?";
+                stm = con.prepareStatement(sql);
+                stm.setString(1, fullname);
+                stm.setString(2, address);
+                stm.setDate(3, dob);
+                stm.setString(4, sex);
+                stm.setString(5, userID);
+                int result = stm.executeUpdate();
+                if (result > 0) {
+                    return true;
+                }
+            }
+        } finally {
+            if (stm != null) {
+                stm.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+        return false;
+    }
 }
